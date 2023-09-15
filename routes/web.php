54894f113b40150e487f4565de9f460930a07156
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-15 07:07:03 +0300

    added admin login dashboard theme

--- routes/web.php
+++ routes/web.php
@@ -23,17 +23,22 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware(['auth', 'role:admin'])->group(function () {
-
-    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
-    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
-}); //end of admin routes
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+}); //end of admin routes
+
+Route::get('admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+
+
+
 
 require __DIR__ . '/auth.php';
