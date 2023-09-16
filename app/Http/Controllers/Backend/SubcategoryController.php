3ef8187000340f38e34c8dde26923e3e697d7f83
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-16 08:59:50 +0300

    add subcategory add method

--- app/Http/Controllers/Backend/SubcategoryController.php
+++ app/Http/Controllers/Backend/SubcategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Subcategory;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class SubcategoryController extends Controller
@@ -14,4 +15,11 @@ class SubcategoryController extends Controller
         $subcategories = Subcategory::latest()->get();
         return view('backend.subcategory.subcategory_all', compact('subcategories'));
     } // End Method
+
+    public function AddSubCategory(){
+
+        $categories = Category::latest()->get();
+        return view('backend.subcategory.subcategory_add',compact('categories'));
+
+    }// End Mehtod
 }
