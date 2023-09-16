8c1b914936ae8960f54415565ba9a2496e067e77
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-16 08:32:54 +0300

    add update category route

--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
         Route::get('/add/category', 'AddCategory')->name('add.category');
         Route::post('/store/category', 'StoreCategory')->name('store.category');
         Route::get('/edit/category/{id}', 'EditCategory')->name('edit.category');
+        Route::post('/update/category', 'UpdateCategory')->name('update.category');
     });
 });
 
