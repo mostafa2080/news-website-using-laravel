5febf0799e5578046aaf94e2915d128e9e76240b
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-16 09:48:57 +0300

    add subcategory update link to edit page form

--- resources/views/backend/subcategory/subcategory_edit.blade.php
+++ resources/views/backend/subcategory/subcategory_edit.blade.php
@@ -29,7 +29,7 @@
                     <div class="card">
                         <div class="card-body">
 
-                            <form id="myForm" method="post" action="">
+                            <form id="myForm" method="post" action="{{ route('update.subcategory') }}">
                                 @csrf
 
                                 <input type="hidden" name="id" value="{{ $subcategory->id }}">
