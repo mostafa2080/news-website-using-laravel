c30c70b3c51060b729bbe0cd7a40d6dad73ffefc
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-16 07:45:18 +0300

    add category controller

---
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function AllCategory(){
+
+        $categories = Category::latest()->get();
+        return view('backend.category.category_all',compact('categories'));
+
+    } // End Mehtod
+}
