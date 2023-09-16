1859590907a63595b6c8bdab539cc08091d85abb
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-16 07:57:16 +0300

    add category store route

--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::controller(CategoryController::class)->group(function () {
         Route::get('/all/category', 'AllCategory')->name('all.category');
         Route::get('/add/category', 'AddCategory')->name('add.category');
+        Route::post('/store/category', 'StoreCategory')->name('store.category');
     });
 });
 
