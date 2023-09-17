bbe41934fa164f44fa8e047f013238e41b19c5ff
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-17 14:49:43 +0300

    add add news post method

--- app/Http/Controllers/Backend/NewsPostController.php
+++ app/Http/Controllers/Backend/NewsPostController.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Models\Subcategory;
 use App\Models\NewsPost;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class NewsPostController extends Controller
@@ -15,6 +16,16 @@ class NewsPostController extends Controller
 
         $allnews = NewsPost::latest()->get();
         return view('backend.news.all_news_post', compact('allnews'));
+    } // End Method
+
+
+    public function AddNewsPost()
+    {
+
+        $categories = Category::latest()->get();
+        $subcategories = Subcategory::latest()->get();
+        $adminuser = User::where('role', 'admin')->latest()->get();
+        return view('backend.news.add_news_post', compact('categories', 'subcategories', 'adminuser'));
     } // End Method
 
 
