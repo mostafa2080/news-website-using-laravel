a0afe5640466a9ed6ebe1214f1704c0d12c6782d
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-17 14:52:27 +0300

    add newspost add route

--- resources/views/backend/news/all_news_post.blade.php
+++ resources/views/backend/news/all_news_post.blade.php
@@ -11,7 +11,7 @@
                     <div class="page-title-box">
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
-                                <a href="{{ route('add.admin') }}" class="btn btn-blue waves-effect waves-light">Add News
+                                <a href="{{ route('add.news.post') }}" class="btn btn-blue waves-effect waves-light">Add News
                                     Post</a>
                             </ol>
                         </div>
