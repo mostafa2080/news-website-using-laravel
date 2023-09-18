8dc5b1dd4004ffdba0d89b07e8f053b760fbef6a
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-18 12:25:31 +0300

    add tags and related news post to newdetails method

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -20,7 +20,12 @@ class IndexController extends Controller
     {
 
         $news = NewsPost::findOrFail($id);
+        $tags = $news->tags;
+        $tags_all = explode(',', $tags);
 
-        return view('frontend.news.news_details', compact('news'));
+        $cat_id = $news->category_id;
+        $relatedNews = NewsPost::where('category_id', $cat_id)->where('id', '!=', $id)->orderBy('id', 'DESC')->limit(6)->get();
+
+        return view('frontend.news.news_details', compact('news', 'tags_all', 'relatedNews'));
     } // End Method
 }
