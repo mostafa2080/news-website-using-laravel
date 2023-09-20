1ed22e2c8313333a9165f2ad9b8f9d52e293daad
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-20 10:12:29 +0300

    add store method

--- app/Http/Controllers/Backend/PhotoGalleryController.php
+++ app/Http/Controllers/Backend/PhotoGalleryController.php
@@ -22,4 +22,30 @@ class PhotoGalleryController extends Controller
     {
         return view('backend.photo.add_photo');
     } // End Method
+
+    public function StorePhotoGallery(Request $request)
+    {
+
+        $image = $request->file('multi_image');
+
+        foreach ($image as $mulit_image) {
+
+            $name_gen = hexdec(uniqid()) . '.' . $mulit_image->getClientOriginalExtension();
+            Image::make($mulit_image)->resize(700, 400)->save('upload/multi/' . $name_gen);
+            $save_url = 'upload/multi/' . $name_gen;
+
+            PhotoGallery::insert([
+                'photo_gallery' => $save_url,
+                'post_date' => Carbon::now()->format('d F Y'),
+
+            ]);
+        } // End Foreach
+
+        $notification = array(
+            'message' => 'Photo Gallery Inserted Successfully',
+            'alert-type' => 'success'
+
+        );
+        return redirect()->route('all.photo.gallery')->with($notification);
+    } // End Method
 }
