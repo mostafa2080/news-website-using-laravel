b208519c0c1ce2fe7835b50936bb01ad05334e29
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-20 05:40:19 +0300

    add alias of google translate

--- config/app.php
+++ config/app.php
@@ -185,7 +185,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'Image' => Intervention\Image\Facades\Image::class
+        'Image' => Intervention\Image\Facades\Image::class,
+        'GoogleTranslate' => Stichoza\GoogleTranslate\GoogleTranslate::class,
 
     ])->toArray(),
 
