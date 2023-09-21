ed89816e2aaf77423efad029533fa25972ac6c74
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-21 11:11:26 +0300

    protect commect section

--- resources/views/frontend/news/news_details.blade.php
+++ resources/views/frontend/news/news_details.blade.php
@@ -135,57 +135,53 @@
 
                 <hr>
 
-                <form action=" " method="post" class="wpcf7-form init" enctype="multipart/form-data"
-                    novalidate="novalidate" data-status="init">
-                    <div style="display: none;">
+                @guest
 
-                    </div>
-                    <div class="main_section">
-                        <div class="row">
-                            <div class="col-md-12 col-sm-12">
-                                <div class="contact-title ">
-                                    Subject *
-                                </div>
-                                <div class="contact-form">
-                                    <span class="wpcf7-form-control-wrap sub_title"><input type="text"
-                                            name="sub_title" value="" size="40"
-                                            class="wpcf7-form-control wpcf7-text" aria-invalid="false"
-                                            placeholder="News Sub Title"></span>
+                    <p><b> For Add Product Review. You Need To Login First <a href="{{ route('login') }}"> Login Page</a> </b>
+                    </p>
+                @else
+                    <form action=" " method="post" class="wpcf7-form init" enctype="multipart/form-data"
+                        novalidate="novalidate" data-status="init">
+                        <div style="display: none;">
+
+                        </div>
+                        <div class="main_section">
+
+
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="contact-title">
+                                        Comments *
+                                    </div>
+                                    <div class="contact-form">
+                                        <span class="wpcf7-form-control-wrap news_details">
+                                            <textarea name="news_details" cols="20" rows="5"
+                                                class="wpcf7-form-control wpcf7-textarea wpcf7-validates-as-required" aria-required="true" aria-invalid="false"
+                                                placeholder="News Details...."></textarea>
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
+
+
+
                         </div>
 
                         <div class="row">
-                            <div class="col-lg-12">
-                                <div class="contact-title">
-                                    Comments *
-                                </div>
-                                <div class="contact-form">
-                                    <span class="wpcf7-form-control-wrap news_details">
-                                        <textarea name="news_details" cols="20" rows="5"
-                                            class="wpcf7-form-control wpcf7-textarea wpcf7-validates-as-required" aria-required="true" aria-invalid="false"
-                                            placeholder="News Details...."></textarea>
-                                    </span>
+                            <div class="col-md-12">
+                                <div class="contact-btn">
+                                    <input type="submit" value="Submit Comments"
+                                        class="wpcf7-form-control has-spinner wpcf7-submit"><span
+                                        class="wpcf7-spinner"></span>
                                 </div>
                             </div>
                         </div>
 
+                        <div class="wpcf7-response-output" aria-hidden="true"></div>
+                    </form>
 
 
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="contact-btn">
-                                <input type="submit" value="Submit Comments"
-                                    class="wpcf7-form-control has-spinner wpcf7-submit"><span
-                                    class="wpcf7-spinner"></span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="wpcf7-response-output" aria-hidden="true"></div>
-                </form>
+                @endguest
 
 
 
