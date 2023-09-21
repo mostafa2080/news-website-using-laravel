c9fef61bee1fee5df8b5c555837d1a12dfb3cb46
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-21 12:05:20 +0300

    add newspost and user relationship to review model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,5 +9,14 @@ class Review extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    public function news()
+    {
+        return $this->belongsTo(NewsPost::class, 'news_id', 'id');
+    }
 
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
