7f56c892441d2d9d832103488cf322ca3f7a6743
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-23 07:04:29 +0300

    add seo setting controller and sitseosetting method

---
+++ app/Http/Controllers/Backend/SeoSettingController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\SeoSetting;
+use Illuminate\Http\Request;
+
+class SeoSettingController extends Controller
+{
+    public function SeoSiteSetting()
+    {
+
+        $seo = SeoSetting::find(1);
+        return view('backend.seo.seo_data', compact('seo'));
+    } // End Method
+
+}
