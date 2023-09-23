91bcdeb3f02e1af17380289913a6f086a421f91e
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-23 11:44:50 +0300

    add edit permission route

--- routes/web.php
+++ routes/web.php
@@ -159,6 +159,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
         Route::get('/all/permission', 'AllPermission')->name('all.permission');
         Route::get('/add/permission', 'AddPermission')->name('add.permission');
         Route::post('/store/permission', 'StorePermission')->name('permission.store');
+        Route::get('/edit/permission/{id}', 'EditPermission')->name('edit.permission');
     });
 }); //end of protected backend routes
 
