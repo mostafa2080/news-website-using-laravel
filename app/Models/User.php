81b5bf00470d4fa51288940e9db3c8aa8a0de836
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-24 10:12:15 +0300

    add getpermissionByGroupName method to usermodel

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,13 @@ class User extends Authenticatable
         $permission_groups = DB::table('permissions')->select('group_name')->groupBy('group_name')->get();
         return $permission_groups;
     }
+
+    public static function getpermissionByGroupName($group_name)
+    {
+        $permissions = DB::table('permissions')
+            ->select('name', 'id')
+            ->where('group_name', $group_name)
+            ->get();
+        return $permissions;
+    }
 }
