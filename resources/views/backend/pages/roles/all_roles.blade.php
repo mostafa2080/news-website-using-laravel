99827e07e7e747f07b6aa2499aed8ae23299c0b4
mostafa2080 <mostafashaaban2080@gmail.com>
2023-09-24 09:32:51 +0300

    add delete role route

--- resources/views/backend/pages/roles/all_roles.blade.php
+++ resources/views/backend/pages/roles/all_roles.blade.php
@@ -45,7 +45,7 @@
                                                 <a href="{{ route('edit.roles', $item->id) }}"
                                                     class="btn btn-primary rounded-pill waves-effect waves-light">Edit</a>
 
-                                                <a href="{{ route('delete.permission', $item->id) }}"
+                                                <a href="{{ route('delete.roles', $item->id) }}"
                                                     class="btn btn-danger rounded-pill waves-effect waves-light"
                                                     id="delete">Delete</a>
 
